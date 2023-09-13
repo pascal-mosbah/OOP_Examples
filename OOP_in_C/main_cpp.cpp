6ac8f77a9e0b7fb10fe0be09548198912cc33114
Pascal MOSBAH <pascal.mosbah@kathab.fr>
2023-09-13 08:44:20 +0200

    Up

--- OOP_in_C/main_cpp.cpp
+++ OOP_in_C/main_cpp.cpp
@@ -6,7 +6,20 @@
 
 int main()
 {
-    auto tab_shape = new Shape_cpp *[_SIZE_]; // Not (so) equivalent to Shape_cpp **tab_shape  = (Shape_cpp **) malloc(_SIZE_ * sizeof(Shape_cpp*))
+    Rectangle rectangle(5.f, 10.f); // length_ = 5.f, width_ = 10.f;
+    printf("aera Shape : %f...\n", rectangle.get_aera());
+    rectangle.print_data();
+
+    auto p_rectangle = &rectangle; // Rectangle *p_rectangle = &rectangle;
+    printf("aera Shape : %f...\n", p_rectangle->get_aera());
+    p_rectangle->print_data();
+
+    Shape_cpp *p_shape = &rectangle;
+    printf("aera Shape : %f...\n", p_shape->get_aera());
+    p_shape->print_data();
+
+    auto tab_shape = new Shape_cpp *[_SIZE_]; 
+    // C++ equivalent to Shape_cpp **tab_shape  = (Shape_cpp **) malloc(_SIZE_ * sizeof(Shape_cpp*)) with some additions
 
     tab_shape[0] = new Rectangle(5, 10);
     tab_shape[1] = new Triangle(5, 12, 15);
@@ -23,4 +36,6 @@ int main()
         delete tab_shape[i];
     }
     delete tab_shape;
+   
+    return EXIT_SUCCESS;
 }
