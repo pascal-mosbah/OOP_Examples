36fe1221e957d131cb5e5fd57a333a2e8a59e6f7
Pascal MOSBAH <pascal.mosbah@kathab.fr>
2023-10-04 10:45:10 +0200

    add CMake_Examples

---
+++ CMake_Examples/project1/src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdlib>
+#include "my_include_file.hpp"
+
+int main()
+{
+	int test = 3;
+	my_function(test);
+	return EXIT_SUCCESS;
+}
---
+++ CMake_Examples/src/myfunction.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "my_include_file.hpp"
+
+void my_function(const int &val)
+{
+	std::cout << "val = " << val << std::endl;
+}
